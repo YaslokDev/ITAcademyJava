74296b6e9bacf61eec313dae3af57f1e3c04aff5
YaslokDev <yako83@gmail.com>
2024-02-28 12:50:13 +0100

    Delete Sprint1 directory

--- Sprint1/n2exercici1/ICamera.java
+++
@@ -1,6 +0,0 @@
-package Sprint1.n2exercici1;
-
-public interface ICamera {
-
-    public void fotografiar();
-}
--- Sprint1/n2exercici1/IRellotge.java
+++
@@ -1,6 +0,0 @@
-package Sprint1.n2exercici1;
-
-public interface IRellotge {
-
-    public void alarma();
-}
